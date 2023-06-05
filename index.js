c5a4955dc9f7bb3515f9aa6ef3b6fdd71ee9d7b1
Rafaysyed <rafaysyed1@gmail.com>
2023-06-06 01:40:11 +0500

    second commit

--- index.js
+++ index.js
@@ -11,35 +11,68 @@ function formatDate(event) {
 }
 // functions for daily time and bullet points 
 
+// function handleKeyPress(event) {
+//   if (event.key === "Enter" || event.keyCode === 13) {
+//     event.preventDefault(); // Prevent the default Enter key behavior
+
+//     const selection = window.getSelection();
+//     const range = selection.getRangeAt(0);
+//     const listItem = document.createElement("li");
+//     listItem.appendChild(range.extractContents());
+//     range.insertNode(listItem);
+//     range.setStartAfter(listItem);
+//     range.setEndAfter(listItem);
+
+//     // Set the cursor position after the inserted bullet point
+//     const newRange = document.createRange();
+//     newRange.setStart(listItem, 1);
+//     newRange.setEnd(listItem, 0);
+//     selection.removeAllRanges();
+//     selection.addRange(newRange);
+//   } else if (event.key.length === 1) {
+//     const selection = window.getSelection();
+//     const range = selection.getRangeAt(0);
+
+//     if (range.startOffset === 0 && range.endOffset === 0) {
+//       const listItem = document.createElement("li");
+//       range.insertNode(listItem);
+//       range.setStart(listItem, 0);
+//       range.setEnd(listItem, 0);
+//       selection.removeAllRanges();
+//       selection.addRange(range);
+//     }
+//   }
+// }
+
+
+
+function setCaretPosition(element, position) {
+  const range = document.createRange();
+  range.setStart(element.firstChild, position);
+  range.collapse(true);
+  const selection = window.getSelection();
+  selection.removeAllRanges();
+  selection.addRange(range);
+}
+
 function handleKeyPress(event) {
+  const td = event.target;
+  const currentCursorPosition = getCaretPosition(td);
+  const text = td.innerText;
+
   if (event.key === "Enter" || event.keyCode === 13) {
-    event.preventDefault(); // Prevent the default Enter key behavior
-
-    const selection = window.getSelection();
-    const range = selection.getRangeAt(0);
-    const listItem = document.createElement("li");
-    listItem.appendChild(range.extractContents());
-    range.insertNode(listItem);
-    range.setStartAfter(listItem);
-    range.setEndAfter(listItem);
-
-    // Set the cursor position after the inserted bullet point
-    const newRange = document.createRange();
-    newRange.setStart(listItem, 1);
-    newRange.setEnd(listItem, 0);
-    selection.removeAllRanges();
-    selection.addRange(newRange);
+    event.preventDefault();
+    const newText =
+      text.slice(0, currentCursorPosition) +
+      "\n→ " +
+      text.slice(currentCursorPosition);
+    td.innerText = newText;
+    setCaretPosition(td, currentCursorPosition + 3);
   } else if (event.key.length === 1) {
-    const selection = window.getSelection();
-    const range = selection.getRangeAt(0);
-
-    if (range.startOffset === 0 && range.endOffset === 0) {
-      const listItem = document.createElement("li");
-      range.insertNode(listItem);
-      range.setStart(listItem, 0);
-      range.setEnd(listItem, 0);
-      selection.removeAllRanges();
-      selection.addRange(range);
+    if (currentCursorPosition === 0) {
+      const newText = "→ " + text;
+      td.innerText = newText;
+      setCaretPosition(td, currentCursorPosition + 2);
     }
   }
 }
@@ -99,24 +132,24 @@ function handleKeyPress(event) {
   });
 
 // function for water tracker
-const addButton = document.querySelector(".add-button");
-const removeButton = document.querySelector(".remove-button");
-const imageContainer = document.querySelector(".image-container");
+const addButton = document.querySelector('.add-button');
+const removeButton = document.querySelector('.remove-button');
+const imageContainer = document.querySelector('.image-container');
 
 const images = [
-  "https://static.vecteezy.com/system/resources/previews/006/599/997/original/flat-design-glass-of-water-isolated-on-white-background-free-vector.jpg",
-  "https://www.coca-colacanada.ca/content/dam/nagbrands/ca/coke/en/specialtysoda/coca-cola-de-mexico/Coca-ColadeMexico355mLBottle-productImageSmall.png",
-  "https://media.istockphoto.com/id/95574723/photo/english-tea-in-a-bone-china-cup.jpg?s=612x612&w=0&k=20&c=ZSdzzGzTz5d5SWXl3Lm5AdRNLWqDmXbmzaO5sqcHiFg=",
-  "https://res.cloudinary.com/sonic-drive-in/image/upload/c_fit,w_600,h_600,dpr_2,f_auto,q_auto/v1504119514/oa_menu/products/menuproduct-minute-maid-100-apple-juice-box-25670-okc-beta.png",
+  'https://static.vecteezy.com/system/resources/previews/006/599/997/original/flat-design-glass-of-water-isolated-on-white-background-free-vector.jpg',
+  'https://www.coca-colacanada.ca/content/dam/nagbrands/ca/coke/en/specialtysoda/coca-cola-de-mexico/Coca-ColadeMexico355mLBottle-productImageSmall.png',
+  'https://media.istockphoto.com/id/95574723/photo/english-tea-in-a-bone-china-cup.jpg?s=612x612&w=0&k=20&c=ZSdzzGzTz5d5SWXl3Lm5AdRNLWqDmXbmzaO5sqcHiFg=',
+  'https://res.cloudinary.com/sonic-drive-in/image/upload/c_fit,w_600,h_600,dpr_2,f_auto,q_auto/v1504119514/oa_menu/products/menuproduct-minute-maid-100-apple-juice-box-25670-okc-beta.png'
 ];
 
 let imageCount = 0;
 let clickCount = 0;
 
-addButton.addEventListener("click", () => {
-  const image = document.createElement("img");
+addButton.addEventListener('click', () => {
+  const image = document.createElement('img');
   image.src = images[imageCount % images.length];
-  image.addEventListener("click", () => {
+  image.addEventListener('click', () => {
     clickCount++;
     if (clickCount % 1 === 0) {
       imageCount++;
@@ -127,7 +160,7 @@ addButton.addEventListener("click", () => {
   imageCount++;
 });
 
-removeButton.addEventListener("click", () => {
+removeButton.addEventListener('click', () => {
   if (imageCount > 0) {
     imageContainer.lastElementChild.remove();
     imageCount--;
